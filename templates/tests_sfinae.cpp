ed0f98fa0b80f54129a645796caf452f9baf57a3
Krystian Piekos <krystian.piekos@infotraining.pl>
2026-06-10 15:14:44 +0200

    SFINAE

--- templates/tests_sfinae.cpp
+++ templates/tests_sfinae.cpp
@@ -1,6 +1,104 @@
 #include <catch2/catch_test_macros.hpp>
 
+namespace Explain
+{
+    template <bool Condition, typename T = void>
+    struct EnableIf
+    {
+        using type = T;
+    };
+
+    template <typename T>
+    struct EnableIf<false, T>
+    { };
+
+    template <bool Condition, typename T = void>
+    using EnableIf_t = typename EnableIf<Condition, T>::type;
+} // namespace Explain
+
+template <typename T>
+auto is_power_of_2(T value) -> std::enable_if_t<std::is_integral_v<T>, bool>
+{
+    return value > 0 && (value & (value - 1)) == 0;
+}
+
+template <typename T>
+auto is_power_of_2(T value) -> std::enable_if_t<std::is_floating_point_v<T>, bool>
+{
+    int exponent;
+    const T mantissa = std::frexp(value, &exponent);
+    return mantissa == static_cast<T>(0.5);
+}
+
+namespace Alternative
+{
+    template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+    bool is_power_of_2(T value)
+    {
+        return value > 0 && (value & (value - 1)) == 0;
+    }
+
+    template <typename T, typename = void, typename = std::enable_if_t<std::is_floating_point_v<T>>>
+    auto is_power_of_2(T value) 
+    {
+        int exponent;
+        const T mantissa = std::frexp(value, &exponent);
+        return mantissa == static_cast<T>(0.5);
+    }
+} // namespace Alternative
+
+namespace Cpp17
+{
+    template <typename T>
+    bool is_power_of_2(T value)
+    {
+        if constexpr (std::is_integral_v<T>)
+        {
+            return value > 0 && (value & (value - 1)) == 0;
+        }
+        else
+        {
+            int exponent;
+            const T mantissa = std::frexp(value, &exponent);
+            return mantissa == static_cast<T>(0.5);
+        }
+    }
+} // namespace Cpp17
+
+namespace Cpp20
+{
+    template <std::integral T>
+    auto is_power_of_2(T value)
+    {
+        return value > 0 && (value & (value - 1)) == 0;
+    }
+
+    template <std::floating_point T>
+    auto is_power_of_2(T value)
+    {
+        int exponent;
+        const T mantissa = std::frexp(value, &exponent);
+        return mantissa == static_cast<T>(0.5);
+    }
+} // namespace Cpp20
+
+// bool is_power_of_2(float value)
+// {
+//     int exponent;
+//     const float mantissa = std::frexp(value, &exponent);
+//     return mantissa == 0.5f;
+// }
+
 TEST_CASE("SFINAE")
-{   
-    // TODO   
+{
+    REQUIRE(is_power_of_2(64));
+    REQUIRE(is_power_of_2(1024L));
+    REQUIRE(is_power_of_2(8));
+    REQUIRE(!is_power_of_2(65ULL));
+    REQUIRE(!is_power_of_2(199));
+
+    REQUIRE(is_power_of_2(8.0));
+    REQUIRE(is_power_of_2(64.0));
+    REQUIRE(!is_power_of_2(111.0f));
+    REQUIRE(is_power_of_2(512.f));
 }
--- templates/tests_traits_and_policies.cpp
+++ templates/tests_traits_and_policies.cpp
@@ -1,4 +1,68 @@
 #include <catch2/catch_test_macros.hpp>
+#include <algorithm>
+#include <iostream>
+
+struct UpperCaseFormatter
+{
+    std::string format(const std::string& message) const
+    {
+        std::string result = message;
+        std::transform(result.begin(), result.end(), 
+                       result.begin(), [](char c) { return std::toupper(c); });   
+        return result;
+    }
+};
+
+struct LowerCaseFormatter
+{
+    std::string format(const std::string& message) const
+    {
+        std::string result = message;
+        std::transform(result.begin(), result.end(), 
+                       result.begin(), [](char c) { return std::tolower(c); });   
+        return result;
+    }
+};
+
+struct CapitalizeFormatter
+{
+    std::string format(const std::string& message) const
+    {
+        std::string result = message;
+        result[0] = std::toupper(result[0]);
+        return result;
+    }
+};
+
+template <typename TFormatter = UpperCaseFormatter>
+class Logger 
+{
+    TFormatter formatter_;
+public:   
+    Logger() = default;
+
+    Logger(TFormatter formatter)
+        : formatter_(std::move(formatter))
+    {
+    }
+
+    void log(const std::string& message)
+    {        
+        std::cout << formatter_.format(message) << std::endl;
+    }
+};
+
+TEST_CASE("static polymorphism")
+{
+    Logger<CapitalizeFormatter> logger_1;
+    logger_1.log("test logger_1");
+
+    Logger<UpperCaseFormatter> logger_2;
+    logger_2.log("text logger_2");
+
+    Logger<LowerCaseFormatter> logger_3;
+    logger_3.log("TEST LOGGER_3");
+}
 
 TEST_CASE("using traits")
 {   
